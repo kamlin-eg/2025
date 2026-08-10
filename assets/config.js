d572fdc6f7e5fba5ffe2114e96dcfc208ef84bc4
kamlin-eg <kamlin.eg2025@gmail.com>
2026-08-10 18:51:26 +0200

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,9 @@
+window.KAMLIN_CONFIG = {
+  companyName: "كاملين للنظافة الفائقة",
+  companyNameEn: "Kamlin for Superior Cleaning Services",
+  phone: "+201030438805",
+  phoneDisplay: "+20 103 043 8805",
+  whatsapp: "+201010420272",
+  whatsappDisplay: "+20 101 042 0272",
+  email: "kamlin.eg2025@gmail.com"
+};
